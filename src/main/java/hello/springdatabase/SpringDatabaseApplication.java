6e035fa067a54ebee098274cfecc1b3964551e8b
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-10 02:48:55 +0900

    feat: 프로필이 test인 경우메나 데이터소스를 스프링 빈으로 등록

--- src/main/java/hello/springdatabase/SpringDatabaseApplication.java
+++ src/main/java/hello/springdatabase/SpringDatabaseApplication.java
@@ -2,12 +2,17 @@ package hello.springdatabase;
 
 import hello.springdatabase.config.JdbcTemplateV3Config;
 import hello.springdatabase.repository.ItemRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
+import javax.sql.DataSource;
+
+@Slf4j
 @Import(JdbcTemplateV3Config.class)
 @SpringBootApplication(scanBasePackages = "hello.springdatabase.web")
 public class SpringDatabaseApplication {
@@ -20,5 +25,19 @@ public class SpringDatabaseApplication {
     @Profile("local")
     public TestDataInit testDataInit(ItemRepository itemRepository) {
         return new TestDataInit(itemRepository);
+    }
+
+    @Bean
+    @Profile("test")
+    public DataSource dataSource() {
+        log.info("🗑️ 메모리 데이터베이스 초기화");
+
+        DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
+        driverManagerDataSource.setDriverClassName("org.h2.Driver");
+        driverManagerDataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
+        driverManagerDataSource.setUsername("sa");
+        driverManagerDataSource.setPassword("");
+
+        return driverManagerDataSource;
     }
 }
