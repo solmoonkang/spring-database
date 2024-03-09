9667716acbc6cf9a3168760b13b47dbd000630cd
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-09 23:08:36 +0900

    feat: JdbcTemplate 연동

--- src/main/java/hello/springdatabase/SpringDatabaseApplication.java
+++ src/main/java/hello/springdatabase/SpringDatabaseApplication.java
@@ -1,12 +1,15 @@
 package hello.springdatabase;
 
+import hello.springdatabase.config.JdbcTemplateV1Config;
 import hello.springdatabase.repository.ItemRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 
-@SpringBootApplication
+@Import(JdbcTemplateV1Config.class)
+@SpringBootApplication(scanBasePackages = "hello.springdatabase.web")
 public class SpringDatabaseApplication {
 
     public static void main(String[] args) {
