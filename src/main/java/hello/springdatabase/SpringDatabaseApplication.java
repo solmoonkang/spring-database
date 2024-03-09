63bd4402c6b0559c4372bef9f810a2ce601a9b9a
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-10 00:02:48 +0900

    feat: JdbcTemplateV2Config.class를 설정으로 사용하도록 변경

--- src/main/java/hello/springdatabase/SpringDatabaseApplication.java
+++ src/main/java/hello/springdatabase/SpringDatabaseApplication.java
@@ -1,6 +1,6 @@
 package hello.springdatabase;
 
-import hello.springdatabase.config.JdbcTemplateV1Config;
+import hello.springdatabase.config.JdbcTemplateV2Config;
 import hello.springdatabase.repository.ItemRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 
-@Import(JdbcTemplateV1Config.class)
+@Import(JdbcTemplateV2Config.class)
 @SpringBootApplication(scanBasePackages = "hello.springdatabase.web")
 public class SpringDatabaseApplication {
 
