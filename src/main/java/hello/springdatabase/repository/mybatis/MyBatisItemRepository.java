1e1c9f48bc0668f9d878a532acf3728067721c88
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-11 11:34:27 +0900

    feat: ItemRepository를 구현한 MyBatisItemRepository 구현

---
+++ src/main/java/hello/springdatabase/repository/mybatis/MyBatisItemRepository.java
@@ -0,0 +1,39 @@
+package hello.springdatabase.repository.mybatis;
+
+import hello.springdatabase.domain.Item;
+import hello.springdatabase.repository.ItemRepository;
+import hello.springdatabase.repository.ItemSearchCond;
+import hello.springdatabase.repository.ItemUpdateDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class MyBatisItemRepository implements ItemRepository {
+
+    private final ItemMapper itemMapper;
+
+    @Override
+    public Item save(Item item) {
+        itemMapper.save(item);
+        return item;
+    }
+
+    @Override
+    public void update(Long itemId, ItemUpdateDto updateParam) {
+        itemMapper.update(itemId, updateParam);
+    }
+
+    @Override
+    public Optional<Item> findById(Long id) {
+        return itemMapper.findById(id);
+    }
+
+    @Override
+    public List<Item> findAll(ItemSearchCond itemSearchCond) {
+        return itemMapper.findAll(itemSearchCond);
+    }
+}
