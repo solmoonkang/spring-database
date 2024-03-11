8a01ac96ae776248c07b3f5514abf8757944f4bf
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-11 16:29:52 +0900

    feat: ItemService가 ItemRepository에 대한 의존을 유지하도록 구현

---
+++ src/main/java/hello/springdatabase/repository/jpa/JpaItemRepositoryV2.java
@@ -0,0 +1,56 @@
+package hello.springdatabase.repository.jpa;
+
+import hello.springdatabase.domain.Item;
+import hello.springdatabase.repository.ItemRepository;
+import hello.springdatabase.repository.ItemSearchCond;
+import hello.springdatabase.repository.ItemUpdateDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+@Transactional
+@RequiredArgsConstructor
+public class JpaItemRepositoryV2 implements ItemRepository {
+
+    private final SpringDataJpaItemRepository repository;
+
+    @Override
+    public Item save(Item item) {
+        return repository.save(item);
+    }
+
+    @Override
+    public void update(Long itemId, ItemUpdateDto updateParam) {
+        Item findItem = repository.findById(itemId).orElseThrow();
+        findItem.setItemName(updateParam.getItemName());
+        findItem.setPrice(updateParam.getPrice());
+        findItem.setQuantity(updateParam.getQuantity());
+    }
+
+    @Override
+    public Optional<Item> findById(Long id) {
+        return repository.findById(id);
+    }
+
+    @Override
+    public List<Item> findAll(ItemSearchCond cond) {
+        String itemName = cond.getItemName();
+        Integer maxPrice = cond.getMaxPrice();
+
+        // 실무에서는 아래 코드와 같이 복잡하지 않고, 동적 쿼리를 사용한다.
+        if (StringUtils.hasText(itemName) && maxPrice != null) {
+            return repository.findItems("%" + itemName + "%", maxPrice);
+        } else if (StringUtils.hasText(itemName)) {
+            return repository.findByItemNameLike("%" + itemName + "%");
+        } else if (maxPrice != null) {
+            return repository.findByPriceLessThanEqual(maxPrice);
+        } else {
+            return repository.findAll();
+        }
+    }
+}
