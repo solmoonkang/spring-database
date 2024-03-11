cd2359541a0cd7ce826937e0e6fa77788451360b
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-11 16:25:11 +0900

    feat: 스프링 데이터 JPA 구현

---
+++ src/main/java/hello/springdatabase/repository/jpa/SpringDataJpaItemRepository.java
@@ -0,0 +1,24 @@
+package hello.springdatabase.repository.jpa;
+
+import hello.springdatabase.domain.Item;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface SpringDataJpaItemRepository extends JpaRepository<Item, Long> {
+
+    List<Item> findByItemNameLike(String itemName);
+
+    List<Item> findByPriceLessThanEqual(Integer price);
+
+
+
+    // Query Method
+    List<Item> findByItemNameLikeAndPriceLessThanEqual(String itemName, Integer price);
+
+    // Query 직접 작성
+    @Query("select i from Item i where i.itemName like :itemName and i.price <= :price")
+    List<Item> findItems(@Param("itemName") String itemName, @Param("price") Integer price);
+}
